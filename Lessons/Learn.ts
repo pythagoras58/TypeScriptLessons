6bddda8a27bf3055be85140d884a6f7a5f859f9c
pythagoras58 <solomondanso58@gmail.com>
2022-12-10 18:31:41 +0000

    Initial commit

---
+++ Lessons/Learn.ts
@@ -0,0 +1,13 @@
+let course = ['java','c++','node']
+
+course.forEach(items=>{
+    console.log(items)
+})
+
+
+function sum(a: number,b:number) {
+    return (a+b);
+}
+
+console.log(sum(45,56));
+
