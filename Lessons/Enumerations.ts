e9e80721bed445865d897c27e3d3241bf5719d4a
pythagoras58 <solomondanso58@gmail.com>
2022-12-10 21:44:04 +0000

    h

--- Lessons/Enumerations.ts
+++ Lessons/Enumerations.ts
@@ -1,9 +1,22 @@
 interface User {
     name : string;
     lastName : string;
+    age? : number;
 }
 
 let user : User = {
     name  : 'Solomon',
-    lastName : 'Danso'
+    lastName : 'Danso',
+    age : 23
 };
+
+let anne : User = {
+    name : 'Anne',
+    lastName : 'Amoah'
+}
+
+document.addEventListener('DOMContentLoaded', function (){
+    document.getElementById('b').innerText = user.name
+    document.getElementById('c').innerText = user.lastName
+    document.getElementById('d').innerText = anne.name
+})
