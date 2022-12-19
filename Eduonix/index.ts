da03560523c70c12e48f48b526199cfc997ac33e
pythagoras58 <solomondanso58@gmail.com>
2022-12-19 22:17:56 +0000

    New course 3

--- Eduonix/index.ts
+++ Eduonix/index.ts
@@ -1,8 +1,16 @@
 // Type inference
-
+const trackingNumber = 'f121A321';
 function getInventoryItem(trackingNumber : string) : {
     displayName:string; inventoryType:string;
     trackingNumber : string; createdDate:Date; orginalCost:number;
 }{
     return null;
 }
+
+let inventoryItem = getInventoryItem(trackingNumber);
+inventoryItem.displayName = 'Pytha-Shop';
+inventoryItem.inventoryType = 'Commerce';
+inventoryItem.createdDate = new Date;
+inventoryItem.orginalCost = 4533;
+
+console.log(inventoryItem);
